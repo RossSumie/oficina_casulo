8c19584cc702b903be4706bb33be8a01264ccbf7
Ross Sumie <rossanasumie@usp.br>
2022-08-07 02:11:38 -0300

    colorful button

--- src/template/Home/DescrptionSection/styles.ts
+++ src/template/Home/DescrptionSection/styles.ts
@@ -20,16 +20,56 @@ export const Container = styled.div`
 `;
 
 export const Button = styled.button`
-    font-size: 2rem;
+    border-radius: 10em;
+    margin-top: 24px;
+    padding: 1.5rem;
+    outline: none;
     color: white;
     width: 20rem;
-    background-color: ${({ theme }) => theme.colors.blue};
+    font-size: 2rem;
     font-family: 'Poppins';
-    padding: 1.5rem;
-    border-radius: 80px;
-    border: solid 3px white;
-    margin-top: 24px;
+    position: relative;
+    z-index: 1;
     cursor: pointer;
+    text-shadow: 3px 3px 10px rgba(0, 0, 0, 0.45);
+    &:before,
+    &:after {
+        position: absolute;
+        top: 50%;
+        left: 50%;
+        border-radius: 10em;
+        transform: translateX(-50%) translateY(-50%);
+        width: 105%;
+        height: 105%;
+        content: '';
+        z-index: -2;
+        background-size: 400% 400%;
+        background: linear-gradient(
+            60deg,
+            #42788c,
+            #623d68,
+            #bf443f,
+            #df7d4e,
+            #e9bb4d,
+            #88a952,
+            #42788c,
+            #623d68,
+            #bf443f
+        );
+    }
+    &:before {
+        transition: all 0.25s ease;
+        animation: pulse 10s infinite ease;
+    }
+    &:after {
+        filter: blur(0.3px);
+    }
+    &:hover {
+        &:before {
+            width: 115%;
+            height: 115%;
+        }
+    }
 `;
 
 export const Text = styled.p`
@@ -48,5 +88,6 @@ export const Wrapper = styled.div`
     background-color: ${({ theme }) => theme.colors.blue};
     @media screen and (max-width: 900px) {
         width: 100vw;
+        padding: 4vw;
     }
 `;
